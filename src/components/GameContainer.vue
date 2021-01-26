1f1584450429608664c3a9e67ddb66d417996fd5
Nate <rewq2502@gmail.com>
2021-01-27 11:11:00 +1300

    Added stages and Option component

--- src/components/GameContainer.vue
+++ src/components/GameContainer.vue
@@ -1,16 +1,34 @@
 <template>
-  <div>{{ initialSearches[0] }}</div>
-  <div>{{ initialSearches[1] }}</div>
+  <div v-if="!stage">
+    <div @click="startGame">Start game</div>
+  </div>
+
+  <div v-if="stage">
+    <Option 
+      v-bind:name="options[0].name" 
+      v-bind:searchCount="options[0].searches" 
+    />
+    <Option 
+      v-bind:name="options[1].name" 
+      v-bind:searchCount="options[1].searches" 
+    />
+  </div>
 </template>
 
 <script>
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { useStore } from "vuex"
 import { shuffleArray } from '../helpers/shuffleArray'
 
+import Option from "./Option.vue"
+
 export default {
   name: "GameContainer",
+  components: {
+    Option
+  },
   setup() {
+    const stage = ref(0)
     const store = useStore()
 
     const initialSearches = computed(() => {
@@ -21,8 +39,16 @@ export default {
       return [first, second]
     })
 
+    const options = ref(initialSearches)
+
+    const startGame = () => {
+      stage.value = 1
+    }
+
     return {
-      initialSearches
+      stage,
+      options,
+      startGame
     }
   }
 }
---
+++ src/components/Option.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ name }}</div>
+  <div>{{ searchCount }}</div>
+</template>
+
+<script>
+export default {
+ name: "Option",
+ props: {
+   name: String,
+   searchCount: Number
+ },
+ setup() {
+   
+ }
+}
+</script>
+
+<style>
+
+</style>
